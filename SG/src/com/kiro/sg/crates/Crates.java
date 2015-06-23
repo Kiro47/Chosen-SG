f9a720713a2e57eb979bdcc9368a15b27813b449
Brandon <kinexkid11@gmail.com>
2015-06-22 22:02:59 -0400

    Crates

---
+++ SG/src/com/kiro/sg/crates/Crates.java
@@ -0,0 +1,37 @@
+package com.kiro.sg.crates;
+
+import org.bukkit.Location;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Crates
+{
+
+	private Map<Location, SupplyCrate> crateMap;
+
+	public Crates()
+	{
+		crateMap = new HashMap<>();
+	}
+
+	public SupplyCrate getCrate(Location location)
+	{
+		SupplyCrate crate = crateMap.get(location);
+		if (crate == null)
+		{
+			crate = new SupplyCrate();
+			crate.populate();
+
+			crateMap.put(location, crate);
+		}
+
+		return crate;
+	}
+
+	public void refillCrates()
+	{
+		crateMap.clear();
+	}
+
+}
---
+++ SG/src/com/kiro/sg/crates/SupplyCrate.java
@@ -0,0 +1,33 @@
+package com.kiro.sg.crates;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class SupplyCrate implements InventoryHolder
+{
+
+	private final Inventory inventory;
+
+	public SupplyCrate()
+	{
+		inventory = Bukkit.createInventory(this, 9, "Supply Crate");
+	}
+
+	public void populate()
+	{
+		// populate Chest
+	}
+
+	public void open(Player player)
+	{
+		player.openInventory(inventory);
+	}
+
+	@Override
+	public Inventory getInventory()
+	{
+		return inventory;
+	}
+}
